a78aee82c2d2773d1e15b8ede0fef6c8e6f1909c
RyoyaFukushima <190146@jc-21.jp>
2022-11-24 14:56:48 +0900

    initial-commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# 概要
+Springでバックエンドを作る時の土台
+# 導入(現段階)
+- git cloneする
+- 直下で```./gradlew bootRun``` or ```gradlew bootRun```する
+- localhost:8080/helloにアクセス
---
+++ src/test/java/com/te4a22/sklm022/Sklm022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.te4a22.sklm022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sklm022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
